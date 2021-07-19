f3b1ac7753c021d796543f00fc059d7ee8701d78
coldpigli <piyushdas98@gmail.com>
2021-07-19 15:58:50 +0530

    Handling error while fetching data

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
 
     useEffect(()=>{
         fetch('http://localhost:8000/blogs')
-        .then((res) => {return res.json();})
+        .then((res) => {
+            if(!res.ok){
+                throw Error('Hmmm...Something seems to be wrong');
+            }
+            
+            return res.json();})
         .then((data)=>{
             setBlogs(data);
-        } )
+            setIsPending(false);
+            setError(null);
+        }).catch((err)=>{
+            setIsPending(false);
+            setError(err.message);
+        })
     },[]);
 
     return (
-        <div className = "home">
+        <div className = "home"> 
+            {error && <p>{error}</p>}
+            {isPending && <p>Loading...</p>}
             {blogs && <BlogList blogs = {blogs} title = "All Blogs!" />}
         </div>
     );
