93c1d1456a463ceddfe8436a84eaa9b87da4a32b
coldpigli <piyushdas98@gmail.com>
2021-07-24 14:11:29 +0530

    Added CSS to the form and set state values to the Inputs

--- src/Create.js
+++ src/Create.js
@@ -1,22 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Coldpigli');
+
     return (
-        <div>
+        <div className = 'create'>
             <h2>Create a New Blog!</h2>
             <form>
                 <label>Blog Title:</label>
                 <input
                     type = "text"
                     required
+                    value = {title}
+                    onChange = {(e)=> setTitle(e.target.value) }
                 />
                 <label>Blog Body:</label>
                 <textarea
                     required   
+                    value = {body}
+                    onChange = {(e) => setBody(e.target.value)}
                 />
                 <label>Blog Author:</label>
-                <select>
-                    <option value = "coldpigli">Coldpigli</option>
-                    <option value = "aru">Aru</option>
-                    <option value = "naugu">Naugu</option>
+                <select 
+                    value = {author}
+                    onChange = {(e) => setAuthor(e.target.value)}>
+                    <option value = "Coldpigli">Coldpigli</option>
+                    <option value = "Aru">Aru</option>
+                    <option value = "Naugu">Naugu</option>
                 </select>
                 <button>
                     Add Blog
