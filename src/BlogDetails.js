eb19b9a72f5a27235f718b7b2010a43fdd3836a6
coldpigli <piyushdas98@gmail.com>
2021-07-24 13:44:38 +0530

    Added a form to enable a new blogpost entry

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const {id} = useParams();
+    const {data: blog, error, isPending} = useFetch("http://localhost:8000/blogs/" + id);
 
     return (
         <div className = "blog-details">
-            <h2>Blog Details of blog # {id}</h2>
+            {isPending &&   <div>Loading...</div> }
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                    <div>{blog.body }</div>
+                </article>
+            )}
         </div>
     );
 };
--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,26 @@ const Create = () => {
     return (
         <div>
             <h2>Create a New Blog!</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type = "text"
+                    required
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required   
+                />
+                <label>Blog Author:</label>
+                <select>
+                    <option value = "coldpigli">Coldpigli</option>
+                    <option value = "aru">Aru</option>
+                    <option value = "naugu">Naugu</option>
+                </select>
+                <button>
+                    Add Blog
+                </button>
+            </form>
         </div>
      );
 
